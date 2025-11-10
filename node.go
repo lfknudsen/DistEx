c725cd66d6416c497e433d3b0947009a166eabd4
Louis <louis@falkknudsen.com>
2025-11-10 14:53:42 +0100

    Made the port number a uint64 to make string conversion simpler, and added a comment to each function.

--- node.go
+++ node.go
@@ -18,7 +18,7 @@ var LamportTime int64 = 0
 
 var State eCriticalSystemState = RELEASED
 
-var Port uint16
+var Port uint64
 
 type Server struct {
 	UnimplementedNodeServer
@@ -33,7 +33,7 @@ func main() {
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", Port))
 	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
@@ -51,13 +51,16 @@ func main() {
 	}
 }
 
+// ReadUserInput runs constantly, reading the standard input.
+// Breaks out when the user types "quit" or "exit", and ignores empty lines.
+// Otherwise, calls [AccessCriticalResource].
 func ReadUserInput(wait chan struct{}) {
 	reader := bufio.NewScanner(os.Stdin)
 	logger.Println("Node started. Enter messages to store in shared file:")
 	for {
 		reader.Scan()
 		if reader.Err() != nil {
-			logger.Fatalf("fail to call Read: %v", reader.Err())
+			logger.Fatalf("failed to call Read: %v", reader.Err())
 		}
 		text := reader.Text()
 		if text == "quit" || text == "exit" {
@@ -67,10 +70,11 @@ func ReadUserInput(wait chan struct{}) {
 		if text == "" {
 			continue
 		}
-		accessCriticalSystem(text)
+		AccessCriticalResource(text)
 	}
 }
 
+// Serve begins the server/service, allowing clients to execute its remote-procedure call functions.
 func Serve(server *grpc.Server, lis net.Listener, wait chan struct{}) {
 	err := server.Serve(lis)
 	if err != nil {
@@ -80,22 +84,23 @@ func Serve(server *grpc.Server, lis net.Listener, wait chan struct{}) {
 	logger.Printf("Listening on %s.\n", lis.Addr())
 }
 
-func accessCriticalSystem(text string) {
+/** AccessCriticalResource appends the given text string to the file called "shared.txt". */
+func AccessCriticalResource(text string) {
 	file, err := os.OpenFile("shared.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		logger.Fatalf("fail to open file: %v", err)
+		logger.Fatalf("failed to open file: %v", err)
 	}
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
-			logger.Fatalf("fail to close file: %v", err)
+			logger.Fatalf("failed to close file: %v", err)
 		}
 	}(file)
 
 	stringToWrite := fmt.Sprintf("%s: %s\n", time.Now().Format(time.DateTime), text)
 	_, err = file.WriteString(stringToWrite)
 	if err != nil {
-		logger.Fatalf("fail to write string: %v", err)
+		logger.Fatalf("failed to write string: %v", err)
 	}
 	logger.Printf("Wrote to critical resource.\n")
 }
@@ -108,8 +113,10 @@ const (
 	HELD     eCriticalSystemState = iota
 )
 
+// InitLogging creates the logging file and initialises the logger with the correct prefix.
 func InitLogging() *os.File {
-	file, err := os.Create("log.txt")
+	filename := fmt.Sprintf("log-%d.txt", Port)
+	file, err := os.Create(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -118,11 +125,14 @@ func InitLogging() *os.File {
 	return file
 }
 
+// ShutdownLogging closes the file which backs the logger.
 func ShutdownLogging(writer *os.File) {
 	logger.Println("Server shut down.")
 	_ = writer.Close()
 }
 
+// ParseArguments reads the command-line arguments and returns the port number specified
+// therein.
 func ParseArguments(args []string) (port uint64) {
 	if len(args) != 2 {
 		log.Fatal("Usage: go run main.go <port>")
